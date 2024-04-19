0164a4a6c0c682ae24fe9da751940e9e606075ce
kanakana-chang <70099904+kanakana-chang@users.noreply.github.com>
2024-04-19 23:14:39 +0900

    render対応

--- app.js
+++ app.js
@@ -3,11 +3,57 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
+const session = require("express-session");
+const passport = require("passport");
+const csurf = require("tiny-csrf");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient({ log: ["query"] });
+
+const GitHubStrategy = require("passport-github2").Strategy;
+const GITHUB_CLIENT_ID = "37138374cbe1a8efc81e";
+const GITHUB_CLIENT_SECRET = "9f2b1400faf78e962c12c6023f7b63aebcb596c5";
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/github/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(async () => {
+        const userId = parseInt(profile.id);
+
+        const data = {
+          userId,
+          username: profile.username,
+        };
+
+        await prisma.user.upsert({
+          where: { userId },
+          create: data,
+          update: data,
+        });
+
+        done(null, profile);
+      });
+    }
+  )
+);
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
+const logoutRouter = require("./routes/logout");
+const schedulesRouter = require("./routes/schedules");
+const availabilitiesRouter = require("./routes/availabilities");
+const commentsRouter = require("./routes/comments");
 
 const app = express();
+app.use(helmet());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -16,11 +62,53 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("nyobiko_signed_cookies"));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "a36ce8a35b6fd3ea",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  csurf(
+    "nyobikosecretsecret9876543212345",
+    ["POST"],
+    [/.*\/(candidates|comments).*/i]
+  )
+);
+
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/login", loginRouter);
+app.use("/logout", logoutRouter);
+app.use("/schedules", schedulesRouter);
+app.use("/schedules", availabilitiesRouter);
+app.use("/schedules", commentsRouter);
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  (req, res) => {
+    const loginFrom = req.cookies.loginFrom;
+    // オープンリダイレクタ脆弱性対策
+    if (loginFrom && loginFrom.startsWith("/")) {
+      res.clearCookie("loginFrom");
+      res.redirect(loginFrom);
+    } else {
+      res.redirect("/");
+    }
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
